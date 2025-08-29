1548cf80c8f33e9633d9faab51a8facae8f117dc
chandravishaal <chandravishal9911.com>
2025-08-29 15:23:50 +0530

    feat: server add and mongo connection

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const connectDB = require("./config/db");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 // Middleware
 app.use(cors());
